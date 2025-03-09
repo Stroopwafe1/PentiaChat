c440c507c42225319c35e74c43b249c6ddce10e5
Stroopwafe1 <48443491+Stroopwafe1@users.noreply.github.com>
2025-03-09 21:43:35 +0100

    Feat: Setup easier dev environment

---
+++ devenv.nix
@@ -0,0 +1,44 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  env.EDGE_PATH = "${pkgs.ungoogled-chromium}";
+
+  # https://devenv.sh/packages/
+  packages = [
+    pkgs.git
+    pkgs.ungoogled-chromium
+    pkgs.jdk17
+    pkgs.firebase-tools
+    pkgs.android-tools
+    pkgs.nodejs_22
+    pkgs.android-studio
+    pkgs.vscodium-fhs
+  ];
+
+  languages.javascript = {
+    enable = true;
+    package = pkgs.nodejs_22;
+    npm.enable = true;
+    npm.install.enable = true;
+    directory = "./PentiaChat";
+  };
+
+  languages.typescript.enable = true;
+
+  languages.java = {
+    enable = true;
+    gradle.enable = true;
+    jdk.package = pkgs.jdk17;
+  };
+
+#   android = {
+#     enable = true;
+#     platforms.version = [ "34" "35" ];
+#     platformTools.version = "34.0.5";
+#     buildTools.version = [ "34.0.0" ];
+#   };
+
+  enterShell = ''
+    codium PentiaChat
+  '';
+}
