e9201b17b6974b83522287705a3fe9bef2e096e6
Stroopwafe1 <48443491+Stroopwafe1@users.noreply.github.com>
2025-03-09 21:27:00 +0100

    Fix: Bug with dark mode
    The statusbar's content colour was inverted

--- PentiaChat/App.tsx
+++ PentiaChat/App.tsx
@@ -3,12 +3,16 @@ import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
 import { FirebaseContextProvider } from './components/providers/FirebaseContextProvider';
 import Navigator from './components/Navigator';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { DarkTheme, DefaultTheme } from '@react-navigation/native';
 
 const App = () => {
 	const isDarkMode = useColorScheme() === 'dark';
+	const theme = useColorScheme() === 'dark' ? DarkTheme : DefaultTheme;
+
 	const style = StyleSheet.create({
 		container: {
-			backgroundColor: isDarkMode ? '#2e2e2e' : '#efefef',
+			backgroundColor: theme.colors.background,
+			color: theme.colors.text,
 			height: '100%',
 			width: '100%',
 		},
@@ -20,7 +24,7 @@ const App = () => {
 				<FirebaseContextProvider>
 					<View style={style.container}>
 						<StatusBar
-						barStyle={!isDarkMode ? 'light-content' : 'dark-content'}
+						barStyle={isDarkMode ? 'light-content' : 'dark-content'}
 						backgroundColor={style.container.backgroundColor}
 						/>
 						<Navigator />
